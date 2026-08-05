316ff3161f2a5b93083150ffc50369a8d966d433
rushilbhat01 <rushilbhat01@gmail.com>
2026-08-05 18:57:06 +0530

    Enable class-based dark mode in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,2 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: { colors: { ink: '#10233f', brand: '#2563eb' } } }, plugins: [] };
+export default { darkMode: 'class', content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: { colors: { ink: '#10233f', brand: '#2563eb' } } }, plugins: [] };
